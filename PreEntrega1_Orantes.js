543fcb1c353d2ebe3ea3b2dae3aec0f5cb46fbfa
Stefany Orantes <stefanysol2338@gmail.com>
2023-08-30 17:46:29 -0600

    2da pre entrega

--- PreEntrega1_Orantes.js
+++ PreEntrega1_Orantes.js
@@ -1,85 +1,122 @@
-/*----------------pre entrega #1*/
+/*----------------pre entrega #2*/
 
-//agrupé los textos para no concatenar tanto
-let hamburguesas=[
+
+let orden=[];
+
+let menu=[
     "1 Hamburguesa de res",
     "2 Hamburguesa de pollo",
     "3 Hamburguesa vegana",
- ];
-let papas=[
     "4 Papas tradicionales",
     "5 Papas con queso",
     "6 Papascon carne y queso",
-];
-let bebidas=[
     "7 Jamaica",
     "8 Té",
     "9 Cocacola",
 ];
+// let hamburguesas=[
+//     "1 Hamburguesa de res",
+//     "2 Hamburguesa de pollo",
+//     "3 Hamburguesa vegana",
+//  ];
+// let papas=[
+//     "4 Papas tradicionales",
+//     "5 Papas con queso",
+//     "6 Papascon carne y queso",
+// ];
+// let bebidas=[
+//     "7 Jamaica",
+//     "8 Té",
+//     "9 Cocacola",
+// ];
 
-let total=0;
+// let total=0;
+// function sumar(x){
+//     total=total+x;
+// }
 
-//la funcion que va a ir sumando lo que se valla ordenando
+function carrito(){
+    push();
+}
+function push(){
+    const count=orden.push(menu[rr-1])
+    alert(orden)
+}
 function sumar(x){
+    let total=0;
     total=total+x;
 }
-
 let rr=0;
-
-//el ciclo se va a repedir hasta que el usuario ingrese un 10, el 10 significa "ya terminé de ordenar"
-for (let i=0; rr!=10; i++){
-
-    //en el mensaje aparece literal "<br>" en lugar de hacer el salto de linea :(
-    rr=parseInt(prompt("Ingresa el numero del producto que quieras ordenar <br>"+ hamburguesas+ "<br"+papas+"<br>"+ bebidas+"</br>10 ya terminé de ordenar"));
-    
-    //un if para cada uno de los posibles ingresos
-    if (rr==1){
-        sumar(10);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==2){
-        sumar(15);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==3){
-        sumar(18);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==4){
-        sumar(5);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==5){
-        sumar(8);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==6){
-        sumar(10);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==7){
-        sumar(8);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==8){
-        sumar(10);
-        alert("el total es: "+total);
-        
-    }
-    else if(rr==9){
-        sumar(12);
-        alert("el total es: "+total);
-        
-    }
-
-
-
+function smore(){
+    do{
+        rr=parseInt(prompt("ingrese el numero del producto que quieres ordenar \n"+menu))
+        carrito()
+    }while(rr!=10);
 }
+
+// function opciones(){
+//     switch(rr){
+//         case 1:
+//             sumar(20)
+//             alert("el total es :" +total)       
+//             break;
+        // case 2:
+        //     sumar(25)
+        //     alert("el total es : "+total)
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (30)
+        //     alert ("el total es: "+total)
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (5)
+        //     alert ("el total es: "+total)
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (8)
+        //     alert ("el total es: "+total) 
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (10)
+        //     alert ("el total es: "+total)
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (10)
+        //     alert ("el total es: "+total)
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (15)
+        //     alert ("el total es: "+total)
+        //     carrito()
+        //     break;
+        // case 3:
+        //     sumar (20)
+        //     alert ("el total es: "+total)
+        //     carrito()
+        //     break;
+//         default:
+//             alert("nothing");
+//     }
+// }
+
+let op;
+op=parseInt(prompt("10 termine de ordenar \n 11 quiero ordenar algo mas \n 12 quiero quitar algo de mi orden \n"));
+switch(op){
+    case 11:
+        smore()
+    break;
+    case 12:
+    alert("quitar algo");
+    break;
+    case 10:
+        alert(orden);
+    break;
+}
+
 alert ("Gracias por su compra");
